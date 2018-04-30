4ed9c372b29b51b08de9752e7f729c00a4f2d0e1
Edward Seley <edwardseley@gmail.com>
2018-04-30 09:06:17 -0400

    Added Command Line Interface

--- poetic_detection.py
+++ poetic_detection.py
@@ -9,6 +9,7 @@ import numpy as np
 import nltk
 from nltk.corpus import stopwords
 import itertools
+import argparse
 
 VOWELS = ['ay', 'ao', 'ih', 'ah', 'eh', 'uw', 'aw', 'aa', 'ae', 'iy', 'er', 'aw', 'eh', 'ow', 'ey', 'uw', 'uh']
 
@@ -32,7 +33,7 @@ def get_rhymes(words):
             if abs(pair[1] - pair[0]) < 15:
                 if last_syllables[pair[1]] != 'oov':
                     if words[pair[0]].word.lower() not in set(stopwords.words('english')) and words[pair[1]].word.lower() not in set(stopwords.words('english')):
-                        rhymes.append(pair)
+                        rhymes.append( ( (words[pair[0]].word, words[pair[0]].phones), (words[pair[1]].word, words[pair[1]].phones) ) )
     return rhymes
 
 '''
@@ -72,25 +73,22 @@ def matching_occurrences(sequence):
         index += 1
     return total_matches
 
-TRANSCRIPT_PATH = "/data/Night_Before_Christmas.txt"
-AUDIO_PATH = "/data/Night_Before_Christmas.mp3"
-
-with open(os.getcwd() + TRANSCRIPT_PATH) as file:
-    transcript = file.read()
-
-resources = gentle.Resources()
-
 '''
-with gentle.resampled(os.getcwd() + AUDIO_PATH) as wavfile:
-    aligner = gentle.ForcedAligner(resources, transcript)
-    result = aligner.transcribe(wavfile)
+Returns a list of all the words (belong to the Word class above) spoken in media files
+'''
+def get_transcribed_words(textFile, audioFile):
+    with open(textFile) as file:
+        transcript = file.read()
 
+    resources = gentle.Resources()
 
-with open('transcription.pkl', 'wb') as output:
+    with gentle.resampled(audioFile) as wavfile:
+        aligner = gentle.ForcedAligner(resources, transcript)
+        result = aligner.transcribe(wavfile)
+
     transcribed_words = []
     for word in result.words:
         phones = word.phones
-        print(word.word)
         if phones is not None:
             root_phones = []
             for phone in phones:
@@ -98,11 +96,50 @@ with open('transcription.pkl', 'wb') as output:
                 root_phones.append(root_phone)
             transcribed_words.append(Word(word.word, root_phones))
 
-    pickle.dump(transcribed_words, output, pickle.HIGHEST_PROTOCOL)
-        #last_syllable =
-        #phoneme_dict[]
-'''
+    return transcribed_words
 
-with open('transcription.pkl', 'rb') as input:
-    transcribed_words = pickle.load(input)
-    get_rhymes(transcribed_words)
+def main():
+
+    parser = argparse.ArgumentParser(
+            description='Using both Kaldi (Automatic Speech Recognition) and Gentle (Forced Word Aligner), \
+            the words of a spoken poem are matched with audio and used to find poetic devices such as alliteration and rhyme.')
+
+    parser.add_argument(
+            '-a',
+            '--audio',
+            type=str,
+            help='audio file of clearly spoken English with minimal background noise',
+            required=True)
+
+    parser.add_argument(
+            '-t',
+            '--text',
+            type=str,
+            help='transcript file of the spoken words used in the audio file',
+            required=True)
+
+    parser.add_argument(
+            '-r',
+            '--rhyme',
+            help="Retrieves all of the rhyming pairs found in the audio and transcript files",
+            required=False,
+            action='store_true'
+    )
+
+    parser.add_argument(
+            '-al',
+            '--alliteration',
+            help="Retrieves all of cases of alliteration found in the audio and transcript files",
+            required=False,
+            action='store_true'
+    )
+
+    args = parser.parse_args()
+
+    transcribed_words = get_transcribed_words(os.getcwd() + args.text, os.getcwd() + args.audio)
+
+    if args.rhyme:
+        print(get_rhymes(transcribed_words))
+
+if __name__ == '__main__':
+    main()
